8007ae95651c9c163f5b487490f02c24b21d059a
NewbyJE <NewbyJE@comcast.net>
2012-10-13 12:01:26 -0700

    miscellaneous: revise prebuilt files and debugging

--- device_tenderloin.mk
+++ device_tenderloin.mk
@@ -159,6 +159,8 @@ PRODUCT_PACKAGES += \
     Mms
 
 PRODUCT_COPY_FILES += \
+   device/hp/tenderloin/aldtf.sh:system/xbin/aldtf.sh \
+   device/hp/tenderloin/xmesg:system/bin/xmesg \
    device/hp/tenderloin/gps.conf:system/etc/gps.conf \
    device/hp/tenderloin/apns-conf.xml:system/etc/apns-conf.xml
 
