eed98eeb2a760e7f6e9c9e1d5c82771828fa5e6a
Hunter X <henry@Henrys-MacBook-Pro.local>
2013-10-26 17:44:29 -0400

    restructure main makefiles
    to comply with default rules

--- cm.mk
+++ cm.mk
@@ -1,5 +1,4 @@
-$(call inherit-product, device/hp/tenderloin/device_tenderloin.mk)
-
+# Release name
 PRODUCT_RELEASE_NAME := Touchpad
 
 # Inherit some common CM stuff.
@@ -8,7 +7,14 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit GSM common stuff
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.1.1/JR003C/228551:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.1.1 JR003C 228551 release-keys"
+# Inherit device configuration
+$(call inherit-product, device/hp/tenderloin/full_tenderloin.mk)
 
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_tenderloin
 PRODUCT_DEVICE := tenderloin
+PRODUCT_BRAND := HP
+PRODUCT_MODEL := TouchPad
+PRODUCT_MANUDFACTURER := HP
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.1.1/JR003C/228551:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.1.1 JR003C 228551 release-keys"
