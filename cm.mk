e4cf226a168b40c20bc2b70cc1ba554be6d6bf24
Hunter X <henry@Henrys-MacBook-Pro.local>
2013-10-26 18:33:37 -0400

    preparing for cm10.1

--- cm.mk
+++ cm.mk
@@ -17,4 +17,4 @@ PRODUCT_BRAND := HP
 PRODUCT_MODEL := TouchPad
 PRODUCT_MANUDFACTURER := HP
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.1.1/JR003C/228551:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.1.1 JR003C 228551 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.2.2 JDQ39 573038 release-keys"
